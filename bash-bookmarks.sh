7de6927adf95c0ebc6ba78106e4c3976b6f717f1
Ryan Peterson <petersonryan97@gmail.com>
2018-08-06 22:59:04 -0700

    Save and print functions save [name] will save current directory with [name] or path name print will list all bookmarks stored

--- bash-bookmarks.sh
+++ bash-bookmarks.sh
@@ -1,1 +1,20 @@
+#marks_name=()
+#marks_path=()
+function save(){
+	marks_path+=($PWD)
+	if [ -z "$1" ]
+		then
+			marks_name+=($PWD)
+		else
+			marks_name+=($1)
+	fi
+	echo "Saved"
+}
+
+function print(){
+	for ((i=0; i<${#marks_name[@]}; i++))
+		do printf "%i: %s\t:\t%s\n" "$i" "${marks_name[$i]}" "${marks_path[$i]}"
+	done
+}
+
 echo "Bash bookmarks!"
