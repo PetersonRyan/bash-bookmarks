17a3173477a86166156defcd693472cba3c0b637
Ryan Peterson <petersonryan97@gmail.com>
2018-08-13 21:57:43 -0700

    Renamed gt to bb for bash-bookmarks Changed save and print options to bbs and bbl as functions

--- bash-bookmarks.sh
+++ bash-bookmarks.sh
@@ -1,64 +1,64 @@
-__gtv_names_arr=()
-__gtv_paths_arr=()
-__gtv_name_save_dir=~/.gt_name_save
-__gtv_path_save_dir=~/.gt_path_save
+__bbv_names_arr=()
+__bbv_paths_arr=()
+__bbv_name_save_dir=~/.bb_name_save
+__bbv_path_save_dir=~/.bb_path_save
 
-function __gtf_save(){
-	__gtv_paths_arr+=($PWD)
+function bbs(){
+	__bbv_paths_arr+=($PWD)
 	if ! [ -z "$1" ] # if $1 not null
 		then #add bookmark name from $1
-			__gtv_names_arr+=($1)
+			__bbv_names_arr+=($1)
 		else
-			__gtv_names_arr+=($PWD) #bookmark name is just directory name
+			__bbv_names_arr+=($PWD) #bookmark name is just directory name
 	fi
 
-	__gtf_file_save "$__gtv_name_save_dir" "${__gtv_names_arr[@]}"
-	__gtf_file_save "$__gtv_path_save_dir" "${__gtv_paths_arr[@]}"
+	__bbf_file_save "$__bbv_name_save_dir" "${__bbv_names_arr[@]}"
+	__bbf_file_save "$__bbv_path_save_dir" "${__bbv_paths_arr[@]}"
 
 	echo "Saved"
 }
 
-function __gtf_print(){
-	for ((i=0; i<${#__gtv_names_arr[@]}; i++))
-		do printf "%i: %s\t:\t%s\n" "$i" "${__gtv_names_arr[$i]}" "${__gtv_paths_arr[$i]}"
+function bbl(){
+	for ((i=0; i<${#__bbv_names_arr[@]}; i++))
+		do printf "%i: %s\t:\t%s\n" "$i" "${__bbv_names_arr[$i]}" "${__bbv_paths_arr[$i]}"
 	done
 	return 0;
 }
 
 # Sets bookmark arrays to empty and deletes the save files
-function __gtf_reset(){
-	__gtv_names_arr=()
-	__gtv_paths_arr=()
-	[ -f "$__gtv_name_save_dir" ] && rm "$__gtv_name_save_dir"
-	[ -f "$__gtv_path_save_dir" ] && rm "$__gtv_path_save_dir"
+function __bbf_reset(){
+	__bbv_names_arr=()
+	__bbv_paths_arr=()
+	[ -f "$__bbv_name_save_dir" ] && rm "$__bbv_name_save_dir"
+	[ -f "$__bbv_path_save_dir" ] && rm "$__bbv_path_save_dir"
 	echo "Bookmarks reset"
 	return 0;
 }
-
-function gt(){
+# bb for bash-bookmarks
+function bb(){
 	if [ $1 = "-h" ]
 		then
 			printf "\n"
-			printf "gt -s <name>\t: Save current directory as <name>\n"
-			printf "gt -s\t\t: Save current directory without name\n"
-			printf "gt -p\t\t: Print all bookmarks\n"
-			printf "gt -r\t\t: Resets bookmarks to nothing\n"
-			printf "gt <index>\t: Go to bookmark with index <index>\n"
+			printf "bbs <name>\t: Save current directory as <name>\n"
+			printf "bb \t\t: Save current directory without name\n"
+			printf "bbl \t\t: Print all bookmarks\n"
+			printf "bb -r\t\t: Resets bookmarks to nothing\n"
+			printf "bb <index>\t: Go to bookmark with index <index>\n"
 			printf "\n"
 			return 0;
 		elif [ $1 = "-p" ]; then
-				__gtf_print; return $?;
+				__bbf_print; return $?;
 		elif [ $1 = '-s' ]; then
-				__gtf_save $2; return $?;
+				__bbf_save $2; return $?;
 		elif [ $1 = '-r' ]; then
-				__gtf_reset; return $?;
+				__bbf_reset; return $?;
 	fi
-	if [[ $(__gtf_is_num $1; echo $?) -eq 0 ]] #if is numeric
+	if [[ $(__bbf_is_num $1; echo $?) -eq 0 ]] #if is numeric
 		then
-			if ! [[ $1 -lt 0 || -z ${__gtv_names_arr[$1]} ]] #if > 0 and index of that not null
+			if ! [[ $1 -lt 0 || -z ${__bbv_names_arr[$1]} ]] #if > 0 and index of that not null
 				then
-					cd ${__gtv_paths_arr[$1]} && return 0;
-					printf "Go to %s : %s has failed" ${__gtv_names_arr[$1]} ${__gtv_paths_arr[$1]}
+					cd ${__bbv_paths_arr[$1]} && return 0;
+					printf "Go to %s : %s has failed" ${__bbv_names_arr[$1]} ${__bbv_paths_arr[$1]}
 				else
 					echo "Argument valid bookmark index or bookmark name"
 			fi
@@ -70,7 +70,7 @@ function gt(){
 # Saves array to file
 # $1 is path to file
 # Pass array as $2 using "${arrayName[@]}"
-function __gtf_file_save(){
+function __bbf_file_save(){
 	[ -z $1 ] && return 1 # Return fail if no path argument
 
 	local arr_to_save=("$@")
@@ -80,34 +80,34 @@ function __gtf_file_save(){
 		if [ "$i" = 1 ]
 		then
 			printf "%s\n" "${arr_to_save[$i]}" > "$1"
-			#printf "%s\t%s\n" "${__gtv_names_arr[$i]}" "${__gtv_paths_arr[$i]}" > "$dir"
+			#printf "%s\t%s\n" "${__bbv_names_arr[$i]}" "${__bbv_paths_arr[$i]}" > "$dir"
 		else
 			printf "%s\n" "${arr_to_save[$i]}" >> "$1"
-			#printf "%s\t%s\n" "${__gtv_names_arr[$i]}" "${__gtv_paths_arr[$i]}" >> "$dir"
+			#printf "%s\t%s\n" "${__bbv_names_arr[$i]}" "${__bbv_paths_arr[$i]}" >> "$dir"
 		fi
 	done
 }
 
-# Reads array from __gtv_name_save_dir to __gtv_names_arr
-function __gtf_read_to_names(){
+# Reads array from __bbv_name_save_dir to __bbv_names_arr
+function __bbf_read_to_names(){
 	let i=0
 	while IFS=$'\n' read -r line_data; do
-			__gtv_names_arr[i]="${line_data}"
+			__bbv_names_arr[i]="${line_data}"
 			((++i))
-	done < "$__gtv_name_save_dir"
+	done < "$__bbv_name_save_dir"
 }
 
-# Reads array from __gtv_path_save_dir to __gtv_paths_arr
-function __gtf_read_to_dirs(){
+# Reads array from __bbv_path_save_dir to __bbv_paths_arr
+function __bbf_read_to_dirs(){
 	let i=0
 	while IFS=$'\n' read -r line_data; do
-	    __gtv_paths_arr[i]="${line_data}"
+	    __bbv_paths_arr[i]="${line_data}"
 	    ((++i))
-	done < "$__gtv_path_save_dir"
+	done < "$__bbv_path_save_dir"
 }
 
 # Returns true if argument is positive or negative integer
-function __gtf_is_num(){
+function __bbf_is_num(){
 	local numReg="^-?[0-9]+$"
 	if [[ $1 =~ $numReg ]] ; then
 			return 0;
@@ -117,7 +117,7 @@ function __gtf_is_num(){
 }
 
 # Read the save files back into memory
-[ -f "$__gtv_name_save_dir" ] && __gtf_read_to_names
-[ -f "$__gtv_path_save_dir" ] && __gtf_read_to_dirs
+[ -f "$__bbv_name_save_dir" ] && __bbf_read_to_names
+[ -f "$__bbv_path_save_dir" ] && __bbf_read_to_dirs
 
 echo "Bash bookmarks!"
